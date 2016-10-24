314f4fe5d54560bfc1495074c51a0c26ca8cb34e
JamesTurman <jturman28@gmail.com>
2016-10-24 14:32:58 -0700

    homework 2

---
+++ hw2/salary.R
@@ -0,0 +1,49 @@
+setwd("C:/Users/jaturman/Desktop/data analysis/ops804")
+sal <- read.csv("salary.csv")
+
+library(reshape2)
+library(dplyr)
+library(e1071)
+library(car)
+library(ggplot2)
+library(reshape)
+
+#assign columns to vectors by sport
+nfl <- c(sal$nfl)
+mlb <- c(sal$mlb)
+nba <- c(sal$nba)
+
+#format data into groups
+tot <- c(nfl,mlb,nba)
+n <- rep(15,3)
+group <- rep(1:3,n)
+
+#summary stats
+tmp = function(x) c(sum=sum(x), mean = mean(x), var=var(x), sd = sd(x),
+                    n = length(x), median=median(x), mode = mode(x), kurtosis=kurtosis(x),
+                    skew = skewness(x), max = max(x), min = min(x), range = max(x)-min(x), 
+                    quartiles = quantile(x),iqr =IQR(x), shapiro=shapiro.test(x)$statistic[1])
+tapply(tot,group,tmp)
+
+#anova
+data <- data.frame(tot=tot, group = factor(group))
+fit <- lm(tot~group, data)
+sal.aov <- aov(fit)
+sal.aov.summary <- summary(sal.aov)
+print(sal.aov.summary)
+
+#perform Tukey-Kramer
+sal.tukey <- TukeyHSD(sal.aov)
+print(sal.tukey)
+
+#perform Levene's test
+sal.lev <- leveneTest(tot~group, data = data)
+print(sal.lev)
+
+
+
+
+
+
+
+
